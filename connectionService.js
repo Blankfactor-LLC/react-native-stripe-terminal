361a7721e25222185a12bf8520eda41171dd2c0d
serhiimahdiuk92 <82449921+serhiimahdiuk92@users.noreply.github.com>
2021-05-10 01:13:44 +0300

    Update connectionService.js

--- connectionService.js
+++ connectionService.js
@@ -1,5 +1,5 @@
 import EventEmitter from "eventemitter3";
-import { AsyncStorage } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function createConnectionService(StripeTerminal, options) {
   class STCS {
